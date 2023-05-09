b2f7e93c412fd1a3cebef1323bdaecf4c6a4ed43
Marco Arath <marco.martinez.morales@u-erre.mx>
2023-05-09 17:02:21 -0600

    ci: organizando los componentes

---
+++ src/components/socialNetworks/SocialNetworks.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import P from '../paragraphLink/ParagraphLink.jsx'
+
+export const SocialNetworks = () => {
+  return (
+    <>
+
+        <P href="https://youtube.com" color='red'> Página Youtube </P>
+        <P href="https://web.whatsapp.com" color='green'> Página Whatsapp </P>
+        <P href="https://twitch.com" color='purple'> Página Twitch </P>
+        <P href="https://mexico.generation.org/" color='blue'> Página Generation </P>
+
+    </>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -8,11 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( 
   <React.StrictMode>
 
-    <P href="https://youtube.com" color='red'> Página Youtube </P>
-    <P href="https://web.whatsapp.com" color='green'> Página Whatsapp </P>
-    <P href="https://twitch.com" color='purple'> Página Twitch </P>
-    <P href="https://mexico.generation.org/" color='blue'> Página Generation </P>
-    <P href="#" > Página CH 26 </P>
+    <App />
 
   </React.StrictMode>
 );
